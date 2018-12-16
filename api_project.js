fe68da64538c8530dd243b47aeac1581759963f7
wl_znzx <wl_znzx@163.com>
2018-12-16 23:49:22 +0800

    frist commit.

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Custom apiDoc browser title",
+  "url": "http://www.alauncher.cn/v1",
+  "name": "接口文档",
+  "version": "0.1.0",
+  "description": "提供遵从 REST 规范的 HTTP API , API 被设计为符合 HTTP，REST 规范。例如：查询请求使用 Get 方法，提交请求使用 Post 方法。如果一个请求不是相应的 HTTP 方法，将返回错误。",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-12-16T12:53:21.315Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
