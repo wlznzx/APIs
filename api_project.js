26eeb4a82151a6185e2489b25d2130a6043fa350
wlznzx <wl_znzx@163.com>
2019-01-23 10:17:15 +0800

    1.更新.

--- api_project.js
+++ api_project.js
@@ -1,6 +1,6 @@
 define({
   "title": "Custom apiDoc browser title",
-  "url": "http://www.alauncher.cn/v1",
+  "url": "http://www.alauncher.cn/via",
   "name": "接口文档",
   "version": "0.1.0",
   "description": "提供遵从 REST 规范的 HTTP API , API 被设计为符合 HTTP，REST 规范。例如：查询请求使用 Get 方法，提交请求使用 Post 方法。如果一个请求不是相应的 HTTP 方法，将返回错误。",
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-16T16:03:15.197Z",
+    "time": "2019-01-23T02:16:21.518Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
